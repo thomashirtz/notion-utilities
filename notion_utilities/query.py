2a2a95fbceff25fabbee18966c19f9310570f744
Thomas <hirtzt@gmail.com>
2022-08-27 20:23:32 +0200

    Add docstring

--- notion_utilities/query.py
+++ notion_utilities/query.py
@@ -7,6 +7,16 @@ def query_database(
         database_id: str,
         page_size: int = 100
 ) -> List[dict]:
+    """Query a Notion database and return the results.
+
+    Args:
+        client: Notion client.
+        database_id: ID of the database.
+        page_size: Number of pages to query at each iteration.
+
+    Returns:
+        List of the pages present in the database.
+    """
     results = []
     query = client.databases.query(
         database_id=database_id, page_size=page_size,
