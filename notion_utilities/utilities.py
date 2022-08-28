d1f6af93908e771580ec30c1c4e68d35e36d040b
Thomas <hirtzt@gmail.com>
2022-08-28 10:21:02 +0200

    Add docstrings

--- notion_utilities/utilities.py
+++ notion_utilities/utilities.py
@@ -3,7 +3,15 @@ import collections
 from typing import Any
 
 
-def get_title(page: dict) -> str:
+def get_title(page: dict) -> str:  # todo simplify code
+    """Return the title of a page when a dictionary of the page is inputted.
+
+    Args:
+        page: Dict of all the Notion page properties.
+
+    Returns:
+        The title of a Notion page.
+    """
     property_name_to_dictionary = page['properties']
     for property_name, dictionary in property_name_to_dictionary.items():
         if dictionary['type'] == 'title':
@@ -11,6 +19,14 @@ def get_title(page: dict) -> str:
 
 
 def is_sequence(obj: Any) -> bool:
+    """Check whether an object is a sequence.
+
+    Args:
+        obj: Any object to inspect.
+
+    Returns:
+        A boolean indicating if the object is a sequence.
+    """
     if isinstance(obj, str):
         return False
     return isinstance(obj, collections.abc.Sequence)  # noqa
